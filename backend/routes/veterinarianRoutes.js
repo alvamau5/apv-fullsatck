c3a4a9111b2eb66fed8d90df07c5150455232dcf
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-06-24 22:50:56 -0600

    making routes for reset passwords of users

--- backend/routes/veterinarianRoutes.js
+++ backend/routes/veterinarianRoutes.js
@@ -1,19 +1,20 @@
 import express from "express";
+const router = express.Router();
 import {
   register,
   profile,
   confirm,
   authenticate
 } from "../controllers/veterinarianController.js";
+import checkAuth from '../middleware/authMiddleware.js'
 
-const router = express.Router();
-
+// public area
 router.post('/', register);
-
-router.get('/profile', profile);
-
 router.get('/confirm/:token', confirm);
-
 router.post('/login', authenticate);
+router.post('/forget-password', forgetPassword);
+
+// private area
+router.get('/profile', checkAuth, profile);
 
 export default router;
