8594c73a79230f1698b165fb02738af14cfe3a86
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-07-06 18:11:33 -0600

    making routes for reset, forget and update passwords of users

--- backend/routes/veterinarianRoutes.js
+++ backend/routes/veterinarianRoutes.js
@@ -4,7 +4,10 @@ import {
   register,
   profile,
   confirm,
-  authenticate
+  authenticate,
+  forgetPassword,
+  checkPassword,
+  newPassword
 } from "../controllers/veterinarianController.js";
 import checkAuth from '../middleware/authMiddleware.js'
 
@@ -13,6 +16,7 @@ router.post('/', register);
 router.get('/confirm/:token', confirm);
 router.post('/login', authenticate);
 router.post('/forget-password', forgetPassword);
+router.route('forget-password/:token').get(checkPassword).post(newPassword);
 
 // private area
 router.get('/profile', checkAuth, profile);
