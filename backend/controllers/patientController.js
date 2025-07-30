59bce9d37eda7e16ede66b6d9185aaef0bd1ff56
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-07-29 23:38:34 -0600

    create functions addPatient and getPatient

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -1,7 +1,27 @@
-const addPatient = (req, res) => {}
-const getPatient = (req, res) => {}
+import Patient from "../modules/Patients.js"
+
+const addPatient = async (req, res) => {
+
+  const patient = new Patient(req.body);
+  patient.veterinarian = req.veterinarian._id;
+
+  try {
+    const patientStored = await patient.save();
+    res.json(patientStored)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getPatients = async (req, res) => {
+  const patients = await Patient.find()
+    .where("veterinario")
+    .equals(req.veterinarian)
+  res.json(patients)
+
+}
 
 export {
   addPatient,
-  getPatient
+  getPatients
 }
