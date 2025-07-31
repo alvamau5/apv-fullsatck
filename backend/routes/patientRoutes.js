d18ddbe774dfdeee5c667f8024ea55dd0cb042b3
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-07-30 23:51:57 -0600

    create routes for getPatient, updatePatient & deletePatient with checkAuth

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -2,12 +2,22 @@ import express from "express"
 const router = express.Router();
 import {
   addPatient,
-  getPatients
+  getPatients,
+  getPatient,
+  updatePatient,
+  deletePatient
 } from "../controllers/patientController.js";
 import checkAuth from "../middleware/authMiddleware.js"
 
-router.route('/')
-  .post(checkAuth, addPatient).
-  get(checkAuth, getPatients);
+router
+  .route('/')
+  .post(checkAuth, addPatient)
+  .get(checkAuth, getPatients);
+
+router
+  .route('/:id')
+  .get(checkAuth, getPatient)
+  .put(checkAuth, updatePatient)
+  .delete(checkAuth, deletePatient)
 
 export default router;
