cfccab35d93710eb9a76cb531b1cfa07fc60a41a
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-08-15 15:35:29 -0600

    makeing page to Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,16 @@
 const Login = () => {
   return (
     <>
+      <div>
+        <h1 className="text-indigo-600 font-black text-6xl">
+          Inicia sesion y administra tus
+          <span className="text-black"> pacientes</span>
+        </h1>
+      </div>
+      <div>
+        2
+      </div>
 
-      <h2>
-        Login
-      </h2>
     </>
   )
 }
