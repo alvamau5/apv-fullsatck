f9a079695ab91a05e7fe40a5749b89dfd386c4aa
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-08-19 22:13:01 -0600

    adding class styles to mokeup

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -9,7 +9,7 @@ import { Outlet } from "react-router-dom"
 const AuthLayout = () => {
   return (
     <>
-      <main className="container mx-auto md:grid md:grid-cols-2">
+      <main className="container mx-auto md:grid md:grid-cols-2 gap-10 p-5">
         <Outlet />
       </main>
     </>
