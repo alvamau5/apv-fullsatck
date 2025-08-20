ad1f93a3f7e8e63351ad12d7273a254f6050cfe9
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-08-19 22:14:15 -0600

    making form to access count

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 const Login = () => {
   return (
     <>
@@ -7,10 +9,57 @@ const Login = () => {
           <span className="text-black"> pacientes</span>
         </h1>
       </div>
-      <div>
-        2
-      </div>
 
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10
+        rounded-xl">
+        <form>
+          <div className="my-5">
+            <label
+              className="uppercase text-gray-600 block text-xl
+              font-bold"
+            >
+              Email
+            </label>
+            <input type="email"
+              placeholder="Email de Registro"
+              className="border w-full p-3 mt-3 bg-gray-50
+              rounded-xl"
+            />
+          </div>
+          <div>
+            <label
+              className="uppercase text-gray-600 block text-xl
+              font-bold"
+            >
+              Password
+            </label>
+            <input type="password"
+              placeholder="Tu Password"
+              className="border w-full p-3 mt-3 bg-gray-50
+              rounded-xl"
+            />
+            <input
+              type="submit"
+              value="Iniciar Sesion"
+              className="bg-indigo-700 w-full py-3 px-10
+              rounded-xl text-white uppercase font-bold mt-5
+              hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+            />
+          </div>
+        </form>
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link
+            className="block text-center my-5 text-gray-500"
+            to="/register">
+            No tienes una cuenta? Registrate
+          </Link>
+          <Link
+            className="block text-center my-5 text-gray-500"
+            to="/forwad-password">
+            Olvide mi password
+          </Link>
+        </nav>
+      </div>
     </>
   )
 }
