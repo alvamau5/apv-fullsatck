a48217b5029749bcf39901863e5a9d37a55c46a3
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-09-11 19:54:19 -0600

    create email register

---
+++ backend/helpers/emailRegister.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+
+const emailRegister = async (data) => {
+
+  const transporter = nodemailer.createTransport({
+    hots: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    }
+  })
+
+  const { email, name, token } = data;
+
+  //Enviar Email
+  const info = await transporter.sendMail({
+    from: '"APV - Administrador de pacientes de veterinario" <apv@corre.com>',
+    to: email,
+    subject: "Comprueba tu cuenta en APV ✔",
+    text: "Comprueba tu cuenta en APV",
+    html: `<p>Hola: ${name}, comprueba tu cuenta APV.</p>
+          <p>Tu cuenta ya esta lista, solo debes comprobarla
+            en el siguiente enlace:
+            <a href="${process.env.FRONTEND_URL}/check-count/${token}"> Comprobar cuenta</a>
+          </p>
+          <p> Si tu no creaste esta cuenta, ingnora el mensaje</p>
+    `
+  })
+
+  console.log("Mensaje enviado: %s", info.messageId);
+}
+
+export default emailRegister
