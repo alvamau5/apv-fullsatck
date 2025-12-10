dc2b10cbc1d6e3cf8a2989561b9fc046a9a47695
alvamau5 <lucioaalvaradomolina@outlook.com>
2025-12-10 11:57:51 -0600

    Create protected routs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layout/AuthLayout"; // Pagina maestra donde se registraran todos los cambios
+import ProtectedRoute from "./layout/ProtectedRoute";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Confirm from "./pages/Confirm";
 import ForgetPassword from "./pages/ForgetPassword";
 import NewPassword from "./pages/NewPassword";
+import PatientAdmin from "./pages/PatientAdmin";
+
 import { AuthProvider } from "./context/AuthProvider";
 
 function App() {
@@ -18,6 +21,11 @@ function App() {
             <Route path="forget-password" element={<ForgetPassword />} />
             <Route path="forget-password/:token" element={<NewPassword />} />
             <Route path="confirm/:id" element={<Confirm />} />
+          </Route>
+
+          {/* Rutas Protegidas */}
+          <Route path="/admin" element={<ProtectedRoute />}>
+            <Route index element={<PatientAdmin />} />
           </Route>
         </Routes>
       </AuthProvider>
