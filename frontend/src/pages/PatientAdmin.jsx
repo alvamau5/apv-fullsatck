5f6290266e13695870243325a9c5b5ac4a473df4
alvamau5 <lucioaalvaradomolina@outlook.com>
2026-02-02 11:43:56 -0600

    File page Patient Admin created

--- frontend/src/pages/PatientAdmin.jsx
+++ frontend/src/pages/PatientAdmin.jsx
@@ -1,13 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import Form from "../components/Form";
+import ListPatients from "../components/ListPatients";
 
 const PatientAdmin = () => {
+  const [showForm, setShowForm] = useState(false);
   return (
-    <>
-      <div>
-        <h1>Administra tus Pacientes</h1>
+    <div className="flex flex-col md:flex-row">
+      <button
+        type="button"
+        className="bg-indigo-600 text-white w-full p-3 rounded-md
+        font-bold hover:bg-indigo-800 cursor-pointer
+        text-center transition colors mb-10 md:hidden"
+        onClick={() => setShowForm(!showForm)}
+      >
+        {showForm ? "Ocultar Formulario" : "Agregar Nuevo Paciente"}
+      </button>
+      <div
+        className={`${showForm ? "block" : "hidden"}
+          md:block md:w-1/2 lg:w-2/5
+          `}
+      >
+        <Form />
       </div>
-      <Outlet />
-    </>
+
+      <div className="md:w-1/2 lg:w-3/5">
+        <ListPatients />
+      </div>
+    </div>
   );
 };
 
